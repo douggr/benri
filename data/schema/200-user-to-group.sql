ecbe99cae619ae92df11df197a8dfe50bfdbf898
Douglas Gontijo <gontijo.douglas@gmail.com>
2014-09-20 13:23:05 -0300

    Moved to follow ZF standards

---
+++ data/schema/200-user-to-group.sql
@@ -0,0 +1,38 @@
+/*
+ * douggr/zf-rest
+ *
+ * @link https://github.com/douggr/zf-rest for the canonical source repository
+ * @version 1.0.0
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file distributed with this source code.
+ */
+
+/*!40101 SET NAMES utf8 */;
+/*!40101 SET GLOBAL log_output = 'TABLE' */;
+/*!40101 SET GLOBAL general_log = 'ON' */;
+
+-- ---------------------------------------------------------------------------
+-- Table structure for table `%DATABASE%`.`user_to_group`
+-- ---------------------------------------------------------------------------
+DROP TABLE IF EXISTS `%DATABASE%`.`user_to_group`;
+CREATE TABLE IF NOT EXISTS `%DATABASE%`.`user_to_group` (
+  `user_id`       INTEGER       NOT NULL,
+  `group_id`      INTEGER       NOT NULL,
+
+  `created_at`    TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by`    INTEGER       NOT NULL,
+  PRIMARY KEY (`user_id`, `group_id`)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB;
+
+ALTER TABLE `%DATABASE%`.`user_to_group`
+  ADD CONSTRAINT `user_to_group_fk_created_by`
+  FOREIGN KEY (`created_by`) REFERENCES `user`(`id`);
+
+ALTER TABLE `%DATABASE%`.`user_to_group`
+  ADD CONSTRAINT `user_to_group_fk_user_id`
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`id`);
+
+ALTER TABLE `%DATABASE%`.`user_to_group`
+  ADD CONSTRAINT `user_to_group_fk_group_id`
+  FOREIGN KEY (`group_id`) REFERENCES `group`(`id`);
---
+++ data/schema/700-user.data.sql
@@ -0,0 +1,35 @@
+/*
+ * douggr/zf-rest
+ *
+ * @link https://github.com/douggr/zf-rest for the canonical source repository
+ * @version 1.0.0
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file distributed with this source code.
+ */
+
+/*!40101 SET NAMES utf8 */;
+/*!40101 SET GLOBAL log_output = 'TABLE' */;
+/*!40101 SET GLOBAL general_log = 'ON' */;
+
+-- ---------------------------------------------------------------------------
+-- Data for table `%DATABASE%`.`user`
+-- ---------------------------------------------------------------------------
+LOCK TABLES `%DATABASE%`.`user` WRITE;
+/*!40000 ALTER TABLE `%DATABASE%`.`user` DISABLE KEYS */;
+INSERT INTO `%DATABASE%`.`user` (
+  `email`,
+  `username`,
+  `password`,
+  `status`,
+  `admin`,
+  `token`,
+  `api_key`,
+  `api_secret`,
+  `visibility`
+) VALUES
+('installer'        , 'installer'        , '$2y$10$P/                                                   ', '0', '0', 'G', 'G                               ', 'G                                                           ', 'PRIVATE'),
+('admin@example.com', 'admin@example.com', '$2y$10$P/KHjeHX8JVRPZ5aLeBxburpGSQ3FNrkQdoP5lLvJH4FZUNuv4EiK', '0', '1', 'A', '43yc3REynzkgn4c4nMcPgMWcxn364EEk', 'YY3K3ss3JqTCLsMk3LdVvWFDLsWUYTD4TYDKM4fJJyDyHDYTdEWdkJskgFKY', 'PRIVATE');
+
+/*!40000 ALTER TABLE `%DATABASE%`.`user` ENABLE KEYS */;
+UNLOCK TABLES;
