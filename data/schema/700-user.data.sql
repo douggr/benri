a1d3106f70b4310d2d48292ef4c6539b60b896f5
Douglas Gontijo <gontijo.douglas@gmail.com>
2014-09-20 14:11:05 -0300

    Removed all detail from user. This should ease master-details references when extending the `user` model.

--- data/schema/700-user.data.sql
+++ data/schema/700-user.data.sql
@@ -21,15 +21,13 @@ INSERT INTO `%DATABASE%`.`user` (
   `email`,
   `username`,
   `password`,
-  `status`,
   `admin`,
   `token`,
   `api_key`,
-  `api_secret`,
-  `visibility`
+  `api_secret`
 ) VALUES
-('installer'        , 'installer'        , '$2y$10$P/                                                   ', '0', '0', 'G', 'G                               ', 'G                                                           ', 'PRIVATE'),
-('admin@example.com', 'admin@example.com', '$2y$10$P/KHjeHX8JVRPZ5aLeBxburpGSQ3FNrkQdoP5lLvJH4FZUNuv4EiK', '0', '1', 'A', '43yc3REynzkgn4c4nMcPgMWcxn364EEk', 'YY3K3ss3JqTCLsMk3LdVvWFDLsWUYTD4TYDKM4fJJyDyHDYTdEWdkJskgFKY', 'PRIVATE');
+('installer'        , 'installer'        , '$2y$10$P/                                                   ', '0', 'G', 'G                               ', 'G                                                           '),
+('admin@example.com', 'admin@example.com', '$2y$10$P/KHjeHX8JVRPZ5aLeBxburpGSQ3FNrkQdoP5lLvJH4FZUNuv4EiK', '1', 'A', '43yc3REynzkgn4c4nMcPgMWcxn364EEk', 'YY3K3ss3JqTCLsMk3LdVvWFDLsWUYTD4TYDKM4fJJyDyHDYTdEWdkJskgFKY');
 
 /*!40000 ALTER TABLE `%DATABASE%`.`user` ENABLE KEYS */;
 UNLOCK TABLES;
