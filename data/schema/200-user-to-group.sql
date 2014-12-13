153fe1b7874fbe2cf026a20c48b950635581e8ea
Douglas Gontijo <gontijo.douglas@gmail.com>
2014-12-13 08:51:01 -0200

    Bump version to 2.0.0.

--- data/schema/200-user-to-group.sql
+++ data/schema/200-user-to-group.sql
@@ -2,7 +2,7 @@
  * douggr/zf-rest
  *
  * @link https://github.com/douggr/zf-rest for the canonical source repository
- * @version 1.1.4
+ * @version 2.0.0
  *
  * For the full copyright and license information, please view the LICENSE
  * file distributed with this source code.
