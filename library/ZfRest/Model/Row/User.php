b944160a08ed2794fa511da730275e4d75cc2528
Douglas Gontijo <gontijo.douglas@gmail.com>
2014-12-20 15:28:59 -0200

    (refactor)
      - validates username on __set
      - added validity for uniqueness on username and email
      - added custom error messages

--- library/ZfRest/Model/Row/User.php
+++ library/ZfRest/Model/Row/User.php
@@ -47,6 +47,11 @@ class ZfRest_Model_Row_User extends ZfRest_Db_Table_Row
      */
     public function setUsername($value)
     {
+        if (preg_match('/^[^a-z]/i', $value)) {
+            // username's must begin with a letter.
+            $this->_pushError('user', 'username', self::ERROR_INVALID, 'username must begin with a letter.');
+        }
+
         return trim($value);
     }
 
@@ -100,6 +105,9 @@ class ZfRest_Model_Row_User extends ZfRest_Db_Table_Row
             $this->_setApiSecret();
         }
 
+        // Never change this…
+        //$this->created_at = $this->_cleanData['created_at'];
+
         return $this;
     }
 
@@ -109,27 +117,32 @@ class ZfRest_Model_Row_User extends ZfRest_Db_Table_Row
     protected function _save()
     {
         if ('' === trim($this->email)) {
-            $this->_pushError('user', 'email', static::ERROR_MISSING_FIELD);
+            $this->_pushError('user', 'email', static::ERROR_MISSING_FIELD, 'email is mandatory');
         }
 
         if ('' === trim($this->username)) {
-            $this->_pushError('user', 'username', static::ERROR_MISSING_FIELD);
+            $this->_pushError('user', 'username', static::ERROR_MISSING_FIELD, 'username is mandatory');
         }
 
         if ('' === trim($this->password)) {
-            $this->_pushError('user', 'password', static::ERROR_MISSING_FIELD);
+            $this->_pushError('user', 'password', static::ERROR_MISSING_FIELD, 'password is mandatory');
         }
 
         if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
-            $this->_pushError('user', 'email', static::ERROR_INVALID);
+            $this->_pushError('user', 'email', static::ERROR_INVALID, 'this email is invalid');
         }
 
         if ($this->isDirty('password')) {
             $this->password = ZfRest_Util_String::password($this->password);
         }
 
-        // Never change this…
-        $this->created_at   = $this->_cleanData['created_at'];
+        if (!$this->_checkModelUniqueness('email')) {
+            return $this->_pushError('user', 'email', static::ERROR_ALREADY_EXISTS, 'email is already taken');
+        }
+
+        if (!$this->_checkModelUniqueness('username')) {
+            return $this->_pushError('user', 'username', static::ERROR_ALREADY_EXISTS, 'username is already taken');
+        }
 
         // … and always change these
         $this->updated_at   = new ZfRest_Util_DateTime();
@@ -145,5 +158,19 @@ class ZfRest_Model_Row_User extends ZfRest_Db_Table_Row
     protected function _setApiSecret()
     {
         $this->api_secret = ZfRest_Util_String::password(ZfRest_Util_String::random(60));
+    }
+
+    /**
+     * 
+     */
+    private function _checkModelUniqueness($column)
+    {
+        $select = $this->select()
+            ->where("$column = ?", $this->$column)
+            ->limit(1);
+
+        $model  = $this->getTable()->fetchRow($select);
+
+        return !$model || $model->id === $this->id;
     }
 }
