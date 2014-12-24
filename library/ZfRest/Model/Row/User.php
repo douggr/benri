20f924c5112c3eebef4dce2dec2cef9284cc59d7
Douglas G.R <gontijo.douglas@gmail.com>
2014-12-24 14:47:18 -0200

    Doh! access_token doesn't have a default value.
      - It's calculated using both api_key and api_secret. Reverted.

--- library/ZfRest/Model/Row/User.php
+++ library/ZfRest/Model/Row/User.php
@@ -158,8 +158,10 @@ class ZfRest_Model_Row_User extends ZfRest_Db_Table_Row
             $this->_pushError('user', 'username', static::ERROR_ALREADY_EXISTS, 'Username is already taken');
         }
 
-        // … and always change this.
-        $this->updated_at = new ZfRest_Util_DateTime();
+        // … and always change there.
+        $this->updated_at   = new ZfRest_Util_DateTime();
+        $this->access_token = null;
+
 
         return $this;
     }
