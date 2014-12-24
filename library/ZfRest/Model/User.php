0174f1d5e68e5c1c604ae8bc081a08ad360eb303
Douglas G.R <gontijo.douglas@gmail.com>
2014-12-24 14:37:14 -0200

    Added findUserById($id) which can find records by username, email or id.
    This is usefull if you use any field as an ID in routes.

--- library/ZfRest/Model/User.php
+++ library/ZfRest/Model/User.php
@@ -23,4 +23,39 @@ class ZfRest_Model_User extends ZfRest_Db_Table_Abstract
      * {@inheritdoc}
      */
     protected $_name = 'user';
+
+    /**
+     * Find a user using various fields as ID.
+     *
+     * @return ZfRest_Model_User_Row
+     */
+    public static function findUserById($id)
+    {
+        if ('new' === $id) {
+            return static::create();
+        }
+
+        if ('me' === $id) {
+            $auth = ZfRest_Auth::getInstance();
+            if ($auth->hasIdentity()) {
+                return static::locate('id', $auth->getIdentity()->id);
+            }
+        }
+
+        if (preg_match('/\d+/', $id)) {
+            $column = 'id';
+        } elseif (filter_var($id, FILTER_VALIDATE_EMAIL)) {
+            $column = 'email';
+        } else {
+            $column = 'username';
+        }
+
+        $model = static::locate($column, $id);
+
+        if (!$model) {
+            return static::create();
+        } else {
+            return $model;
+        }
+    }
 }
