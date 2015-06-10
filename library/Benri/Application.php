fa4644a87d7435b0b9a4aef0dc8f0f285b637178
Douglas Gontijo <douggr@users.noreply.github.com>
2015-06-10 19:31:51 -0300

    (feat) This might ease your life.
    Signed-off-by: Douglas Gontijo <douggr@users.noreply.github.com>

---
+++ library/Benri/Application.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @link http://framework.zend.com/manual/1.12/en/zend.application.html Zend_Application
+ */
+class Benri_Application extends Zend_Application
+{
+    /**
+     * Singleton instance
+     *
+     * @var Benri_Application
+     */
+    protected static $_instance;
+
+
+    /**
+     * Returns an instance of Benri_Application
+     *
+     * Singleton pattern implementation
+     *
+     * @return Benri_Application
+     */
+    public static function getInstance()
+    {
+        return self::$_instance;
+    }
+
+
+    /**
+     * Create and returns an instance of Benri_Application
+     */
+    public static function createInstance($environment, $options = null)
+    {
+        if (!self::$_instance) {
+            self::$_instance = new static($environment, $options, false);
+        }
+
+        return self::$_instance;
+    }
+}
